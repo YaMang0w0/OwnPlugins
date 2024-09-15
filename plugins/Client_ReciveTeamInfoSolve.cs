46dc7afa0e2f974d58f2648cefb3bdbb86f02cf9
YaMang -w- <98645233+YaMang0w0@users.noreply.github.com>
2024-09-15 19:26:49 +0900

    Add files via upload
    Disconnected: RPC Error in CLIENT_ReciveTeamInfo: An item with the same key has already been added. Key: steamid at ...
    I encountered a problem and created a plugin to fix it.

---
+++ plugins/Client_ReciveTeamInfoSolve.cs
@@ -0,0 +1,26 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Client_ReciveTeamInfoSolve", "YaMang -w-", "1.0.0")]
+    [Description("'CLIENT_ReciveTeamInfo: An Item with the same key has already been added. Key: error solve plugins'")]
+    public class Client_ReciveTeamInfoSolve : RustPlugin
+    {
+        // Work for me 2024-09-15
+        private void OnPlayerConnected(BasePlayer player)
+        {
+            if (player.UserIDString != "steamid") return;
+            if(player.currentTeam == 0)
+            {
+                PrintWarning($"{player.displayName} No Team idk");
+            }
+            else
+            {
+                PrintWarning($"{player.displayName} Have Team | Team Disband!");
+
+                player.Team.Disband();
+                
+                player.SendNetworkUpdate();
+                player.SendNetworkUpdateImmediate();
+            }
+        }
+    }
+}
